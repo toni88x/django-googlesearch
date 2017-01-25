fea6a7a978543fb45fd9b4e02af9733930f56653
Toni Engelhardt <toni88x@gmail.com>
2017-01-25 21:27:39 +0100

    Fix memetype and render_to_string

--- googlesearch/views.py
+++ googlesearch/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.shortcuts import render
 
 
 def cref_cse(request):
     return HttpResponse(
-        loader.render_to_string('googlesearch/cref_cse.xml', {}, context_instance=RequestContext(request)),
-        mimetype = 'text/xml'
+        render(request, 'googlesearch/cref_cse.xml', {}),
+        content_type='text/xml',
     )
