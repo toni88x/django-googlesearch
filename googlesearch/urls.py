1dac5f0dd43de31e4a549b7a342a1f344d03bb75
Toni Engelhardt <toni88x@gmail.com>
2017-01-25 21:16:33 +0100

    Fix view reference in urls

--- googlesearch/urls.py
+++ googlesearch/urls.py
@@ -1,19 +1,18 @@
 from django.conf.urls import url
 from django.views.generic.base import TemplateView
 
+from .views import cref_cse
+
 
 urlpatterns = [
 
-    url(
-        r'^results/$',
+    url(r'^results/$',
         TemplateView.as_view(template_name='googlesearch/results.html'),
         name='googlesearch-results'
-    ),
-
-    url(
-        r'^cref-cse\.xml/$',
-        'googlesearch.views.cref_cse',
+        ),
+    url(r'^cref-cse\.xml/$',
+        cref_cse,
         {},
         name='googlesearch-cref-cse'
-    ),
+        ),
 ]
